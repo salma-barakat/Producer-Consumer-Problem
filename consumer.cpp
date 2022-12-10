6580eb975772052d76b9eda95ad2719b7add2657
salma-barakat <salmabarakat2015@gmail.com>
2022-12-10 22:35:38 +0200

    shared memory working

--- consumer.cpp
+++ consumer.cpp
@@ -2,28 +2,33 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
 #include <iostream>
 using namespace std;
 
 
 int main()
 {
+  int i;
+  void *shared_memory;
+  char buff[100];
     // ftok to generate unique key
     key_t key = ftok("shmfile",65);
 
     // shmget returns an identifier in shmid
-    int shmid = shmget(key,1024,0666|IPC_CREAT);
+    // shmget used to create the shared memory segment
+    int shmid = shmget(key,1024,0666);
+    cout<<"Key of shared memory is "<<shmid<<endl;
 
-    // shmat to attach to shared memory
-    char *str = (char*) shmat(shmid,(void*)0,0);
-
-    cout<<"Consume: ";
-    cin>>str;
-
-    printf("Currency : %s\n",str);
+    // shmat to attach the shared segment with the address space of the process
+    shared_memory=shmat(shmid,NULL,0);
+    cout<<"Process attached at "<<shared_memory<<endl;
+    printf("Data read from shared memory is : %s\n",(char *)shared_memory);
 
     //detach from shared memory
-    shmdt(str);
+    //shmdt(str);
 
     return 0;
 }
--- producer.cpp
+++ producer.cpp
@@ -2,27 +2,43 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
 using namespace std;
-  
+
+struct buff{
+char* name;
+float priceMean;
+float standardDev;
+int sleepInterval;
+};
+
 int main()
 {
-    // ftok to generate unique key
-    key_t key = ftok("shmfile",65);
+  int i;
+  void *shared_memory;
+  char buff[100];
+    // ftok to generate unique key
+    key_t key = ftok("shmfile",65);
 //msqid = msgget(ftok("/tmp", 'A'), (IPC_CREAT | IPC_EXCL | 0400));
-  
-    // shmget returns an identifier in shmid
-    int shmid = shmget(key,1024,0666|IPC_CREAT);
-  
-    // shmat to attach to shared memory
-    char *str = (char*) shmat(shmid,(void*)0,0);
-  
-    cout<<"Produce : ";
-    cin>>str;
-  
-    printf("Data written in memory: %s\n",str);
-      
-    //detach from shared memory 
-    shmdt(str);
-  
-    return 0;
+
+    // shmget returns an identifier in shmid
+    // shmget used to create the shared memory segment
+    int shmid = shmget(key,1024,0666|IPC_CREAT);
+    cout<<"Key of shared memory is "<<shmid<<endl;
+
+// shmat to attach the shared segment with the address space of the process
+    shared_memory=shmat(shmid,NULL,0);
+    cout<<"Process attached at "<<shared_memory<<endl;
+
+    read(0,buff, 100); //get input from user
+    strcpy((char *)shared_memory,buff); //data written to shared memory
+    cout<<"You wrote: "<<(char *)shared_memory<<endl;
+    //printf("You wrote : %s\n",(char *)shared_memory);
+
+    //detach from shared memory
+    //shmdt(str);
+
+    return 0;
 }
