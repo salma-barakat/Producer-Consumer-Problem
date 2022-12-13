8653fb00012a3119d250a21ff598633ce68f90c0
salma-barakat <55440159+salma-barakat@users.noreply.github.com>
2022-12-13 23:02:54 +0200

    array of commodites needs fixing

--- consumer.cpp
+++ consumer.cpp
@@ -14,12 +14,15 @@
 #include <signal.h>
 #include <iostream>
 #include <random>
+#include <iterator>
+#include <queue>
 using namespace std; 
 struct consumer{
 	char name[10];//1 byte
-	double price;//8 byte
+	double currentPrice;//8 byte
 	int index;//4yte	
 	double avgPrice = 0; //8 byte 
+	queue<double> prevPrices;
 };
 union semun{
 	int val;
@@ -38,7 +41,7 @@ union semun{
 };*/
 char name1[10];
 double price;
-#define size 5
+#define Size 5
 int r,i=0;
 int main(){
 	struct sembuf sem_buf;
@@ -56,7 +59,7 @@ int main(){
 	int x0=semctl(semid0,0,SETVAL,sem_val);
 	sem_val.val=0;
 	x0=semctl(semid1,0,SETVAL,sem_val);
-	sem_val.val=size;
+	sem_val.val=Size;
 	x0=semctl(semid2,0,SETVAL,sem_val);
 	}else{
 	if(semid0==-1 and errno==EEXIST ){
@@ -69,13 +72,14 @@ int main(){
 		semid2=semget(key2,1,0); 	
 	}
 	}
-	int shmid =shmget(key3,size,0666 | IPC_CREAT);
+	int shmid =shmget(key3,Size,0666 | IPC_CREAT);
 	consumer *buffer=(consumer *)shmat(shmid,0,0);
-	vector<struct consumer> alumin;
+	//vector<struct consumer> commodities;
+	struct consumer commodities[11];
 	consumer c;
-	int al = 0;
-	int less4 = 1;
-	char arrow;
+	int comm;
+	//int less4 = 1;
+	char *arrow;
 	//buffer[size];
 	while(true){
 		sem_buf.sem_op=-1;	//wait for n 
@@ -87,13 +91,50 @@ int main(){
 		sem_buf.sem_flg=SEM_UNDO;
 		r=semop(semid0,&sem_buf,1);
 		//cs..
+		cout<<"line 91"<<endl;
 		strcpy(c.name,buffer[i].name);
-		c.price=buffer[i].price;
-		alumin.push_back(c);
-		i=(i+1)%size;
+		c.currentPrice=buffer[i].currentPrice;
+		cout<<"line 94"<<endl;
+		if(strcmp(c.name, "aluminium"))
+			comm = 0;
+		else if(strcmp(c.name, "copper"))
+			comm = 1;
+		else if(strcmp(c.name, "cotton"))
+			comm = 2;
+		else if(strcmp(c.name, "crudeoil"))
+			comm = 3;
+		else if(strcmp(c.name, "gold"))
+			comm = 4;
+		else if(strcmp(c.name, "lead"))
+			comm = 5;
+		else if(strcmp(c.name, "menthaoil"))
+			comm = 6;
+		else if(strcmp(c.name, "naturalgas"))
+			comm = 7;
+		else if(strcmp(c.name, "nickel"))
+			comm = 8;
+		else if(strcmp(c.name, "silver"))
+			comm = 9;
+		else if(strcmp(c.name, "zinc"))
+			comm = 10;
+		if(commodities[comm].prevPrices.size()==4){
+			commodities[comm].prevPrices.pop();
+		}
+		cout<<"line 98"<<endl;
+		commodities[comm] = c;
+		cout<<"line 100"<<endl;
+		commodities[comm].prevPrices.push(c.currentPrice);
+		queue<double> temp = commodities[comm].prevPrices;
+		while (!temp.empty()) {
+			cout << temp.front()<<" "<<endl;
+			temp.pop();
+		}
+		cout<<"line 102	"<<endl;
+		i=(i+1)%Size;
 		//cout<<buffer->index<<endl;
 		//buffer->index=(buffer->index+1)%size;
-		cout<<alumin[al].name<<" "<<alumin[al].price<<endl;
+		cout<<commodities[comm].name<<" "<<commodities[comm].currentPrice<<endl;
+		cout<<"line 107"<<endl;
 		//al++;
 
 	 
@@ -107,36 +148,54 @@ int main(){
 		sem_buf.sem_flg=SEM_UNDO;
 		r=semop(semid2,&sem_buf,1);
 
-		if(strcmp(c.name, "aluminium") == 0){
-      	cout<<"line 116"<<endl;
-		if(al == 4){
-			cout<<"line 118"<<endl;
-			alumin.erase(alumin.begin());
-			al --;
-		}
-		if(al == 3)
-			less4 = 0;
-		if(!less4){
-			for(int i=0; i<=al; i++){
-			cout<<"line 124"<<endl;
-			alumin[al].avgPrice += alumin[i].price;
-			cout<<"line 126"<<endl;
-			}
-		alumin[al].avgPrice /= (al + 1);
-		}
-		// if(al != 0){
-		// 	if(alumin[al].price > alumin[al-1].price)
-		// 		arrow = ''
+		// if(comm == 4){
+		// 	cout<<"line 122"<<endl;
+		// 	commodities.erase(commodities.begin());
+		// 	comm --;
 		// }
+		// if(al == 3)
+		// 	less4 = 0;
+		cout<<"size"<<commodities[comm].prevPrices.size()<<endl;
+		if(commodities[comm].prevPrices.size() == 4){
+			cout<<"line 130"<<endl;
+			queue<double> temp = commodities[comm].prevPrices;
+			while (!temp.empty()) {
+				commodities[comm].avgPrice += temp.front();
+				//cout << temp.front()<<" "<<endl;
+				temp.pop();
+			}
+		commodities[comm].avgPrice /= (comm + 1);
+		cout<<"line 135"<<endl;
+		}
+		/*
+		if(al != 0){
+			if(commodities[al].price > commodities[al-1].price)
+			{
+				cout<<"before"<<endl;
+				printf("%c",24);
+				strcpy(arrow, " ↑ ");
+				cout<<"after"<<endl;
+			}
+				
+			// else if(commodities[al].price < commodities[al-1].price)
+			// 	strcpy(arrow," ↓ ");
+			// else
+			// 	strcpy(arrow, " - ");
+		}*/
 		
 		cout<<"success"<<endl;
 
 		cout<<"+-------------------------------------+"<<endl;
 		cout<<"| Currency | Price | AvgPrice |"<<endl;
-		cout<<"+-------------------------------------+"<<endl;
-		cout<<"| "<<alumin[al].name <<"| "<<alumin[al].price <<"| "<<alumin[al].avgPrice<<" |"<<endl;
-		al++;
-	  }
+		for(int k=0; k<11; k++){
+			cout<<"+-------------------------------------+"<<endl;
+			cout<<"| "<<commodities[k].name;
+			printf("|%7.2lf %c ", commodities[k].currentPrice, 216);
+			printf("|%7.2lf|\n",commodities[k].avgPrice);
+			cout<<"+-------------------------------------+"<<endl;
+		}
+		
+	  
 
 		 
 		
