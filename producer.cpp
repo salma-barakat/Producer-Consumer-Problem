be9f0a0919c289bb1d1e167f9baefd4d29426547
Yasmin-Eshra <112403956+yasmin-ashraf-eshra@users.noreply.github.com>
2022-12-13 20:41:24 +0200

    output pro

--- producer.cpp
+++ producer.cpp
@@ -16,6 +16,7 @@
 #include <random>
 #include <chrono>
 #include <iomanip>
+#include <time.h>
 using namespace std; 
 
 struct producer{
@@ -29,15 +30,29 @@ union semun{
 	unsigned short int *array;
 	struct seminfo *__buf;
 };
-#define size 5
-int intervalsec,r;
+int intervalsec,r,N;
 double mean,deviation,pricee;
 char name[10];
+struct timespec ts;
+time_t ti;
+struct tm *tinfo;
+int calc_nano(){
+		ti=time(0);
+		tinfo=localtime(&ti);
+		clock_gettime(CLOCK_REALTIME,&ts);
+		//stringstream ss;
+		//ss << ts.tv_nsec;
+		//string inp=ss.str();
+		//string nano=inp.substr(3,inp.length());
+		//return stoi(nano)/1000;
+		return ts.tv_nsec;
+}
 int main(int argc,char *argv[]){
 	strcpy(name,argv[1]);
 	mean=stod(argv[2]);
 	deviation=stod(argv[3]);
 	intervalsec=stoi(argv[4]);
+	N=stoi(argv[5]);
 	default_random_engine generator;
 	normal_distribution<double>distribution(mean,pow(deviation,2));
 	struct sembuf sem_buf;
@@ -49,27 +64,8 @@ int main(int argc,char *argv[]){
 	int semid0=semget(key0,1,IPC_EXCL |0666); //s
 	int semid1=semget(key1,1,IPC_EXCL |0666); //n
 	int semid2=semget(key2,1,IPC_EXCL |0666); //e
-	//sem_val.val=1;
-	//int x0=semctl(semid0,0,SETVAL,sem_val);
-	//sem_val.val=0;
-	//x0=semctl(semid1,0,SETVAL,sem_val);
-	//sem_val.val=size;
-	//x0=semctl(semid2,0,SETVAL,sem_val);
-	int shmid =shmget(key3,size,0666|IPC_CREAT);
-    //generating a new value:
-    auto now = std::chrono::high_resolution_clock::now();
-    auto time = std::chrono::system_clock::to_time_t(now);
-    auto tm = *std::gmtime(&time);
-    auto epoch = now.time_since_epoch();
-    auto us = std::chrono::duration_cast<std::chrono::microseconds>(epoch).count() % 1000000;
-
-    cout << std::put_time(&tm, "%F %T.") << us << std::put_time(&tm, " %Z") << name<<
-    "Generating a new value "<<shmid<<endl;
-
-
-
+	int shmid =shmget(key3,N*sizeof(producer),0666|IPC_CREAT);
 	producer *buffer=(producer *)shmat(shmid,0,0);
-	//buffer[size];
 	if(semid0==-1 and errno==EEXIST ){
 		semid0=semget(key0,1,0); 	
 	}
@@ -80,28 +76,43 @@ int main(int argc,char *argv[]){
 		semid2=semget(key2,1,0); 	
 	}
 	while(true){
-		cout<<"e:"<<semctl(semid2,0,GETVAL,sem_val)<<endl;;
-		cout<<semid0<<endl;
-		cout<<semid1<<endl;
-		cout<<semid2<<endl;
 		pricee=distribution(generator);
+		 //generating a new value:
+    		auto now = std::chrono::high_resolution_clock::now();
+    		auto timee = std::chrono::system_clock::to_time_t(now);
+    		auto tm = *std::localtime(&timee);
+		int nano=calc_nano();
+		cout <<"["<<put_time(&tm, "%m/%d/%Y %T.")<<nano<<"] "<<name<<": Generating a new value "<<pricee<<endl;
 		//sem_op -ve,block until enough resources -> wait
 		//sem_op +ve,realse resource-> signal
 		sem_buf.sem_op=-1;	//wait for e -> number of empty cells
 		sem_buf.sem_num=0;
 		sem_buf.sem_flg=SEM_UNDO;
 		r= semop(semid2,&sem_buf,1);
+		
+		now = std::chrono::high_resolution_clock::now();
+    		timee = std::chrono::system_clock::to_time_t(now);
+    		tm = *std::localtime(&timee);
+		nano=calc_nano();
+		cout <<"["<<put_time(&tm, "%m/%d/%Y %T.")<<nano<<"] "<<name<<": trying to get mutex on shared buffer "<<endl;
+		
 		sem_buf.sem_op=-1;	//wait for s
 		sem_buf.sem_num=0;
 		sem_buf.sem_flg=SEM_UNDO;
 		r=semop(semid0,&sem_buf,1);
 		//cs
+		now = std::chrono::high_resolution_clock::now();
+    		timee = std::chrono::system_clock::to_time_t(now);
+    		tm = *std::localtime(&timee);
+		nano=calc_nano();
+		cout <<"["<<put_time(&tm, "%m/%d/%Y %T.")<<nano<<"] "<<name<<": placing "<<pricee<<" on shared buffer"<<endl;
+		
 		strcpy(buffer[buffer->index].name,name);
 		buffer[buffer->index].price=pricee;
-		cout<<buffer[buffer->index].name<<endl;
-		cout<<buffer[buffer->index].price<<endl;
-		cout<<buffer->index<<endl;
-		buffer->index=(buffer->index+1)%size;
+		//cout<<buffer[buffer->index].name<<endl;
+		//cout<<buffer[buffer->index].price<<endl;
+		//cout<<buffer->index<<endl;
+		buffer->index=(buffer->index+1)%N;
 		//...
 		sem_buf.sem_op=1;	//signal for s
 		sem_buf.sem_num=0;
@@ -111,15 +122,13 @@ int main(int argc,char *argv[]){
 		sem_buf.sem_num=0;
 		sem_buf.sem_flg=SEM_UNDO;
 		r=semop(semid1,&sem_buf,1);
-		cout<<"....."<<endl;
-        auto now = std::chrono::high_resolution_clock::now();
-        auto time = std::chrono::system_clock::to_time_t(now);
-        auto tm = *std::gmtime(&time);
-        auto epoch = now.time_since_epoch();
-        auto us = std::chrono::duration_cast<std::chrono::microseconds>(epoch).count() % 1000000;
-
-        cout << std::put_time(&tm, "%F %T.") << us << std::put_time(&tm, " %Z") <<
-        "Sleeping for "<<intervalsec <<" ms"<<endl;
+		
+		now = std::chrono::high_resolution_clock::now();
+    		timee = std::chrono::system_clock::to_time_t(now);
+    		tm = *std::localtime(&timee);
+		nano=calc_nano();
+		cout <<"["<<put_time(&tm, "%m/%d/%Y %T.")<<nano<<"] "<<name<<": sleeping for "<<intervalsec<<"ms"<<endl;
+		
 		sleep(intervalsec/1000);
 	}
 	return 0;
